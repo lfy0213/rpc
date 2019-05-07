1544bf8da541a1ac2d409d956e2fdb1f65220dbb
zhuroufu <zhuroufu@2dfire.com>
2019-05-08 01:18:12 +0800

    init

---
+++ src/main/java/com/moxiaosheng/rpc/Hello.java
@@ -0,0 +1,13 @@
+package com.moxiaosheng.rpc;
+
+/**
+ * @Desc
+ * @Author fuyou
+ * @Date 2019/5/8 1:11
+ **/
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
